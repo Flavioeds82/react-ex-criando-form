01f3300fe0809fad87c9aa0303bd004cb4464ce8
Flavioeds82 <flavio_eduardo@id.uff.br>
2022-05-21 11:19:43 -0300

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,48 @@
+import React, { useState } from "react";
 const App = () => {
+
+
+  const [name, setName] = useState('');
+  const [lastname, setLastName] = useState('');
+  const [age, setAge] = useState(0);
+
+  function typingName(event: React.ChangeEvent<HTMLInputElement>){
+    return setName(event.target.value);
+  }
+  function typingLastName(event: React.ChangeEvent<HTMLInputElement>){
+    return setLastName(event.target.value);
+  }
+  function typingAge(event: React.ChangeEvent<HTMLInputElement>){
+    return setAge( parseInt(event.target.value));
+  }
+
+
   return (
-    <div>
+    <div className="container">
+      <div className="headline">
+        Formulário
+      </div>
       <div>
-        Nome:
-        <input type="text" />
+        Nome: <br />
+        <input className="input" type="text" value={name} onChange={typingName} />
       </div>
 
       <div>
-        Sobrenome:
-        <input type="text" />
+        Sobrenome: <br />
+        <input className="input" type="text" value={lastname} onChange={typingLastName} />
       </div>
 
       <div>
-        Idade:
-        <input type="text" />
+        Idade: <br />
+        <input className="input" type="text" value={age} onChange={typingAge} defaultValue='0' />
       </div>
 
       <hr/>
-
-      Olá ---, tudo bem?<br/>
-      Você tem -- anos.
+      <div className="texto">
+        Olá {name} {lastname}, tudo bem?<br/>
+        Você tem {age} anos.
+      </div>
+      
     </div>
   );
 }
